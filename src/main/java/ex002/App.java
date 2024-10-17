2844f5575f664a39194314786505310202f3814e
Yago de Lima França <yagodlfa@gmail.com>
2024-10-17 20:55:45 -0300

    ex002 done

---
+++ src/main/java/ex002/App.java
@@ -0,0 +1,10 @@
+package ex002;
+
+public class App {
+    public static void main(String[] args) {
+        int n1 = 36;
+        int n2 = 74;
+        int res = n1 + n2;
+        System.out.println(res);
+    }
+}
---
+++ src/main/java/ex002/README.md
@@ -0,0 +1,8 @@
+# Question 2
+
+### Write a Java program to print the sum of two numbers.
+
+Test Data:  
+74 + 36  
+Expected Output :
+110
