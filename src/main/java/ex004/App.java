baec9d8e70a44ebcc953f1b5ddcfea4db8ea8075
Yago de Lima França <yagodlfa@gmail.com>
2024-10-17 21:15:17 -0300

    ex005 done

---
+++ src/main/java/ex004/App.java
@@ -0,0 +1,15 @@
+package ex004;
+
+public class App {
+    public static void main(String[] args) {
+        int res = -5 + 8 * 6;
+        System.out.println(res);
+        res = (55 + 9) % 9;
+        System.out.println(res);
+        res = 20 + (-3 * 5) / 8;
+        System.out.println(res);
+        res = 5 + 15 / 3 * 2 - 8 % 3;
+        System.out.println(res);
+    }
+
+}
