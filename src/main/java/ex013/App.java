1c100c4db12226044c63f3c0668d49dab4a50f1e
Yago de Lima França <yagodlfa@gmail.com>
2024-10-19 11:09:17 -0300

    ex013 done

---
+++ src/main/java/ex013/App.java
@@ -0,0 +1,14 @@
+package ex013;
+
+public class App {
+    public static void main(String[] args) {
+
+        double width = 5.6;
+        double height = 8.5;
+
+        System.out.println("Area is " + width + " * " + height + " = " + (width * height));
+        System.out.println("Perimeter 2 * (" + width + " + " + height + ") = " + (2 * (width + height)));
+
+    }
+
+}
