90aa851251c6dc393c5c72147e17cde4efd298ee
Yago de Lima França <yagodlfa@gmail.com>
2024-10-21 20:16:47 -0300

    ex015 done

--- src/main/java/ex015/App.java
+++ src/main/java/ex015/App.java
@@ -2,12 +2,27 @@ package ex015;
 
 public class App {
     public static void main(String[] args) {
+        String firstBinaryNumber = "10";
+        String secondBinaryNumber = "11";
 
-        System.out.println(" +\"\"\"\"\"+ ");
-        System.out.println("[| o o |]");
-        System.out.println(" |  ^  | ");
-        System.out.println(" | '-' | ");
-        System.out.println(" +-----+ ");
+        int n1 = 0;
+        int n2 = 0;
+
+        n1 = binaryConversor(firstBinaryNumber);
+        n2 = binaryConversor(secondBinaryNumber);
+
+        System.out.printf("Sum of two binary numbers: %s%n", Integer.toBinaryString(n1 + n2));
+
     }
+
+    private static int binaryConversor(String binaryNumber) {
+        int n = 0;
+        for (int i = 0; i < binaryNumber.length() ; i++) {
+            char b = binaryNumber.charAt(i);
+            n += (int) ((b - '0') * Math.pow(2,binaryNumber.length() - 1 - i));
+        }
+        return n;
+    }
+
 
 }
--- src/main/java/ex015/README.md
+++ src/main/java/ex015/README.md
@@ -1,12 +1,9 @@
 # Question 15
 
-### Write a Java program to print a face.
-Expected Output
+### Write a Java program to add two binary numbers.
+Input Data:  
+Input first binary number: 10  
+Input second binary number: 11  
+Expected Output  
 
-````
- +"""""+                                                 
-[| o o |]                                                
- |  ^  |                                                 
- | '-' |                                                 
- +-----+
-````
+Sum of two binary numbers: 101
