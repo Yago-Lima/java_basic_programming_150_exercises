af6e30fedf1a08e338fe957dcbe051fad0b32fd6
Yago de Lima França <yagodlfa@gmail.com>
2024-10-21 21:00:58 -0300

    ex020 done

---
+++ src/main/java/ex020/App.java
@@ -0,0 +1,9 @@
+package ex020;
+
+public class App {
+    public static void main(String[] args) {
+        int number = 15;
+        System.out.println("Hexadecimal number is: "+Integer.toHexString(number));
+    }
+
+}
---
+++ src/main/java/ex020/README.md
@@ -0,0 +1,8 @@
+# Question 20
+
+### Write a Java program to convert a decimal number to a hexadecimal number.
+Input Data:  
+Input a decimal number: 15  
+Expected Output  
+
+Hexadecimal number is : F
