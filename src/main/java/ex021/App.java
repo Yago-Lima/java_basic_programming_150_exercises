157579069ee5609e79ced96fa7f25afc1a1e1049
Yago de Lima França <yagodlfa@gmail.com>
2024-10-23 18:01:48 -0300

    ex021 done

---
+++ src/main/java/ex021/App.java
@@ -0,0 +1,9 @@
+package ex021;
+
+public class App {
+
+    public static void main(String[] args) {
+        int number = 15;
+        System.out.println(Integer.toOctalString(number));
+    }
+}
---
+++ src/main/java/ex021/README.md
@@ -0,0 +1,8 @@
+# Question 21
+
+### Write a Java program to convert a decimal number to an octal number.
+Input Data:  
+Input a Decimal Number: 15  
+Expected Output  
+
+Octal number is: 17  
